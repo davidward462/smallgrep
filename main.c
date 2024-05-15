da6fe220e366617a6ba0eee74a3b0ca753a09501
David Ward <davidmward@mail.com>
2024-05-14 23:29:38 -0700

    get command line arguments

--- main.c
+++ main.c
@@ -2,5 +2,16 @@
 
 int main(int argc, char** argv)
 {
+        if (argc < 3) {
+                // TODO: print this to stderr and exit with the correct error value
+                printf("usage: ./grep <query> <filename>\n");
+                return 0;
+        }
+
+        // Store variables from file arguments
+        char* program_name = argv[0];
+        char* query = argv[1];
+        char* filename = argv[2];
+
         return 0;
 }

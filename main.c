141e5762228e80528109b5dbe27d84362b02387f
David Ward <davidmward@mail.com>
2024-05-15 22:12:35 -0700

    open file and check for error simply

--- main.c
+++ main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv)
 {
         if (argc < 3) {
                 // TODO: print this to stderr and exit with the correct error value
                 printf("usage: ./grep <query> <filename>\n");
-                return 0;
+                return 1;
         }
 
         // Store variables from file arguments
         char* program_name = argv[0];
         char* query = argv[1];
         char* filename = argv[2];
+
+        // open file
+        FILE *file = fopen(filename, "r");
+
+        // check if file was opened correctly
+        if (file == NULL) {
+                printf("Error opening file.\n");
+                return 1;
+        }
+
+        printf("opened file %s\n", filename);
+
 
         return 0;
 }
